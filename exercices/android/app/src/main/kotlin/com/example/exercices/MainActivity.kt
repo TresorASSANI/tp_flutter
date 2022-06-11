540286212ba1e7d0cbec4b4adaf3f6a2540b7055
TresorASSANI <104302229+TresorASSANI@users.noreply.github.com>
2022-06-11 03:02:10 -0700

    modification du projet

--- exercices/android/app/src/main/kotlin/com/example/exercices/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.exercices
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ tp_graphique/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:tp_graphique/personne.dart';
+import 'package:tp_graphique/update.dart';
+import 'package:tp_graphique/deleteP.dart';
+import 'package:flutter/material.dart';
+import 'dart:math' as Math;
+
+void main() {
+  // runApp(MaterialApp(home: Identite()));
+  //runApp(MaterialApp(home: EditPersonne()));
+  //runApp(MaterialApp(home: DeletePersonne()));
+  //runApp(MaterialApp(home: Personne()));
+}
